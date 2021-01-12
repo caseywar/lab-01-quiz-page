6a238b4ef778c10bb96cc8056d4de99ae6b36c64
Casey Warren <caseywar1017@gmail.com>
2021-01-11 19:34:20 -0500

    id elements pulled and working. quiz button working

--- app.js
+++ app.js
@@ -1,5 +1,28 @@
 // import functions and grab DOM elements
+import { countsAsAYes } from './utils.js';
 
-// initialize state
+const quizButton = document.getElementById('quiz-button');
+const resultSpace = document.getElementById('result-space')
+
+// console.log(quizButton);
+// console.log(resultSpace);
+
 
 // set event listeners to update state and DOM
+
+quizButton.addEventListener('click', () => {
+    // post-click logic goes here!
+    // alert('Welcome to the Casey Quiz!');
+    // const proceed = confirm('Do you want to proceed?');
+
+    // if (!proceed) return;
+    // let correctAnswers = 0;
+
+    // const firstName = prompt('What is your first name?');
+    // const lastName = prompt('Last Name?');
+    // const firstAnswer = prompt('Has Casey lived in Kalamazoo for 10 years?');
+
+    // if (countsAsAYes(firstAnswer)) correctAnswers++;
+    console.log('sup');
+
+});
